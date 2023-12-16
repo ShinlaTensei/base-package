233e6852f5b0948d11ef7bad7cff0890666c2519
phongth <45889679+ShinlaTensei@users.noreply.github.com>
2023-12-16 20:08:05 +0700

    Update TaskRunner Add AnimatorExtension

--- Runtime/Helper/Utilities/TaskRunner.cs
+++ Runtime/Helper/Utilities/TaskRunner.cs
@@ -7,9 +7,12 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Base.Logging;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Base.Helper
 {
@@ -18,6 +21,56 @@ namespace Base.Helper
     /// </summary>
     public static class TaskRunner
     {
+        private static IDictionary<string, CancellationTokenSource> m_savedCancellationToken = new Dictionary<string, CancellationTokenSource>();
+
+        private static void LazyInitializeCancellationToken(string typeName)
+        {
+            if (!m_savedCancellationToken.ContainsKey(typeName))
+            {
+                m_savedCancellationToken.Add(typeName, new CancellationTokenSource());
+            }
+            else
+            {
+                m_savedCancellationToken[typeName] = new CancellationTokenSource();
+            }
+        }
+
+        public static CancellationToken GetCancellationTokenForType(string typeName)
+        {
+            if (string.IsNullOrEmpty(typeName))
+            {
+                PDebug.Warn("TaskRunner: Trying to get a UniTask cancellation token. Type name is empty");
+                return default;
+            }
+            
+            LazyInitializeCancellationToken(typeName);
+
+            return m_savedCancellationToken[typeName].Token;
+        }
+
+        public static void CancelTrackedTask(string typeName)
+        {
+            if (!m_savedCancellationToken.ContainsKey(typeName))
+            {
+                return;
+            }
+            
+            m_savedCancellationToken[typeName]?.Cancel();
+            m_savedCancellationToken[typeName] = null;
+            m_savedCancellationToken.Remove(typeName);
+        }
+
+        public static void CancelAllTrackedTask()
+        {
+            PDebug.Info($"{nameof(TaskRunner)}: Cancel all tracked task");
+            foreach (var token in m_savedCancellationToken)
+            {
+                token.Value?.Cancel();
+            }
+            
+            m_savedCancellationToken.Clear();
+        }
+        
         private static async UniTask Run(Task task, Action onComplete = null, Action<Exception> onError = null)
         {
             try
@@ -97,5 +150,36 @@ namespace Base.Helper
         {
             Run(task, onComplete, onError).Forget();
         }
+
+        public static UniTask WaitUntil(string callerName, Func<bool> predicate,  PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            return UniTask.WaitUntil(predicate, timing, GetCancellationTokenForType(callerName));
+        }
+
+        public static UniTask WaitUntil(Func<bool> predicate, CancellationToken ct, PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            return UniTask.WaitUntil(predicate, timing, ct);
+        }
+        
+        public static UniTask WaitUntil(Func<bool> predicate, PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            return UniTask.WaitUntil(predicate, timing);
+        }
+
+        public static UniTask WaitWhile(string callerName, Func<bool> predicate, PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            return UniTask.WaitWhile(predicate, timing, GetCancellationTokenForType(callerName));
+        }
+        
+        public static UniTask WaitWhile(Func<bool> predicate, CancellationToken ct, PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            return UniTask.WaitWhile(predicate, timing, ct);
+        }
+        
+        public static UniTask WaitWhile(Func<bool> predicate, PlayerLoopTiming timing = PlayerLoopTiming.Update)
+        {
+            return UniTask.WaitWhile(predicate, timing);
+        }
+        
     }
 }
